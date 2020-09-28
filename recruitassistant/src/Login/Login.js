d28cebc7e4549af362fc1d9a72b047735ece0bfa
calvinwang9 <mushy0705@gmail.com>
2020-09-28 21:30:11 +1000

    added basic new page

---
+++ recruitassistant/src/Login/Login.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
+import {Card, Container} from 'react-bootstrap';
+
+
+function Login() {
+	const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+	return (
+		<div>hello</div>
+	)
+}
+
+export default Login;
