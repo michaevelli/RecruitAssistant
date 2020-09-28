2b918172058e37b5d47d3ae56da4e1de008d68fa
calvinwang9 <mushy0705@gmail.com>
2020-09-28 21:58:28 +1000

    added basic router

--- recruitassistant/src/App.js
+++ recruitassistant/src/App.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Login from './Login/Login.js';
+import {Router,Route} from 'react-router-dom';
+import history from './History';
+import Login from './Login/Login';
+
 
 function App() {
   return (
-    <Login/>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div>
+      <div>home page</div>
+      <Router history={history}>
+        <Route path="/login" component={Login}/>
+      </Router>
+    </div>
+    
   );
 }
 
