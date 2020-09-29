5cabd20d27c11d2a71d15e9cf706689a1232086c
beccaD6 <becca.diaz6@gmail.com>
2020-09-29 15:41:56 +1000

    sign up page

--- recruitassistant/src/SignUp/SignUp.js
+++ recruitassistant/src/SignUp/SignUp.js
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
 import {Card, Container} from 'react-bootstrap';
-
+import {Link} from 'react-router-dom';
 
 export default function SignUp() {
 	
-  const [user_type, setPassword] = useState("");
+  const [userType, setUserType] = useState("");
 
 	return (
-		<div>
-            <h1> RecruitAssistant </h1>
-            <h> Sign Up </h>
-            <p> I am a ...</p>
-            <Button variant="contained">Recruiter</Button>
-            <Button variant="contained">Job Seeker</Button>
+        <div>
+        <h1 style={{'margin-left': 50}} > RecruitAssistant </h1>
+		<Container style={{'textAlign': 'center', 'margin':100}}>
+            
+             <div >
+                    
+                    <h> Sign Up </h>
+                    <p> I am a ...</p>
+            </div>
+            <form>
+            <Button 
+            component={Link} to='/RecruiterSignUp' 
+            variant="contained"
+            style={{"margin":10}}>Recruiter</Button>
+            <Button 
+            component={Link} to='/JobSeekerSignUp' 
+            variant="contained"
+            style={{"margin":10}}>Job Seeker</Button>
 
-
+            </form> 
+        </Container>
         </div>
 	)
 }
