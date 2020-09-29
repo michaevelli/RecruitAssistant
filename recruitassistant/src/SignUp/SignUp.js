5429ae58b92c61556697b05b301bbd68d8f5e9cc
beccaD6 <becca.diaz6@gmail.com>
2020-09-29 19:51:40 +1000

    align title

--- recruitassistant/src/SignUp/SignUp.js
+++ recruitassistant/src/SignUp/SignUp.js
@@ -1,22 +1,25 @@
 import React, { useState } from "react";
-import { TextField, Button } from "@material-ui/core";
+import { TextField, Button, Grid } from "@material-ui/core";
 import {Card, Container} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
-
+import Typography from '@material-ui/core/Typography';
 export default function SignUp() {
 	
-  const [userType, setUserType] = useState("");
+        //const [userType, setUserType] = useState("");
 
 	return (
-        <div>
-        <h1 style={{'margin-left': 50}} > RecruitAssistant </h1>
-        <h> Sign Up </h>
-	<Container style={{'textAlign': 'center', 'margin':100}}>
-            
-             <div >       
-                    <p> I am a ...</p>
-            </div>
-            <form>
+                <Container style={{'textAlign': 'center'}}>
+               <Typography variant="h3" component="h2" gutterBottom>
+                 RecruitAssistant
+                </Typography>
+                <p>Sign Up</p>
+
+                <Container style={{'textAlign': 'center', 'margin':70}}>
+                
+                <div >       
+                        <p> I am a ...</p>
+                </div>
+             
                 <Button 
                 component={Link} to='/RecruiterSignUp' 
                 variant="contained"
@@ -26,9 +29,8 @@ export default function SignUp() {
                 variant="contained"
                 style={{"margin":10}}>Job Seeker</Button>
 
-            </form> 
+                </Container>
         </Container>
-        </div>
 	)
 }
 
