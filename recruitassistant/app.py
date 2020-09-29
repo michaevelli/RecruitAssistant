9776ae56f8c6e7b182fe61fe96b6cea2a4f92400
Brendon <bbrendonshi@gmail.com>
2020-09-29 16:25:29 +1000

    firebase connected and basic registration setup

--- recruitassistant/app.py
+++ recruitassistant/app.py
@@ -1,26 +1,57 @@
 import time
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify 
 import firebase_admin
-from firebase_admin import db
-
+from firebase_admin import credentials, auth
+import pyrebase
+import json
 
 app = Flask(__name__)
 
-firebase_admin.initialize_app(options={
-    'databaseURL': 'https://recruitassistant-fe71e.firebaseio.com/'
-})
-RADB = db.reference('recruitassistant')
+# connect to firebase
+cred = credentials.Certificate('./backend/recruitassistant_cred.json')
+firebase = firebase_admin.initialize_app(cred)
+pb = pyrebase.initialize_app(json.load(open('./backend/firebase_config.json')))
 
-
+# test
 @app.route('/time')
 def get_current_time():
     data = {'time': 10000}
     return jsonify(data)
 
-@app.route('/testlogin')
-def test_login():
-    data = {'username': 'hi'}
-    d1 = RADB.push(req)
-    return flask.jsonify({'id': d1.key}), 201
+@app.route('/api/seeker_signup')
+def seeker_signup():
+    #fullname = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or password is None:
+        return jsonify({'message': 'Error missing email or password'}),400
+    try:
+        user = auth.create_user(
+                #fullname=fullname,
+                email=email,
+                password=password
+        )
+        return jsonify({'message': f'Successfully created user {user.uid}'}),200
+    except:
+        return jsonify({'message': 'Error creating user'}),400
 
+@app.route('/api/recruiter_signup')
+def recruiter_signup():
+    fullname = request.form.get('name')
+    company = request.form.get('company_name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if fullname is None or email is None or password is None:
+        return jsonify({'message': 'Error missing email or password'}),400
+    try:
+        user = auth.create_user(
+                type="recruiter",
+                fullname=fullname,
+                company=company,
+                email=email,
+                password=password
+        )
+        return jsonify({'message': f'Successfully created recruiter {user.uid}'}),200
+    except:
+        return jsonify({'message': 'Error creating recruiter'}),400
