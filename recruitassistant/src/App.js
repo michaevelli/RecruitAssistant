1dab46137a7e2721f03340f8c7b5ab34172f8aee
CattleyaMai <z5206096@ad.unsw.edu.au>
2020-09-29 18:52:28 +1000

    Sign up pages for recruiter and jobseeker.

--- recruitassistant/src/App.js
+++ recruitassistant/src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import {Router,Route} from 'react-router-dom';
 import history from './History';
 import Login from './Login/Login';
-
+import SignUpRecruiter from './SignUp/SignUpRecruiter';
+import SignUpJobSeeker from './SignUp/SignUpJobSeeker';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
@@ -18,6 +19,8 @@ function App() {
       <p>The current time is {currentTime}.</p>
       <Router history={history}>
         <Route path="/login" component={Login}/>
+        <Route path="/signuprecruiter" component={SignUpRecruiter}/>
+        <Route path="/signupjobseeker" component={SignUpJobSeeker}/>
       </Router>
     </div>
     
