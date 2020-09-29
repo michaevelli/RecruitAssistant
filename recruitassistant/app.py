930fa6b17357a9801e195e90cd4cb42a815bf7bc
Brendon <bbrendonshi@gmail.com>
2020-09-29 15:05:00 +1000

    jsonify fix

--- recruitassistant/app.py
+++ recruitassistant/app.py
@@ -1,8 +1,10 @@
 import time
 from flask import Flask
+from flask import jsonify 
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
-    return {'time': 10000}
+    data = {'time': 10000}
+    return jsonify(data)
