483cf51fe7bf1ceb06ef9fd9f8cbe014231ced08
Brendon <bbrendonshi@gmail.com>
2020-09-29 15:25:53 +1000

    initial firebase start

--- recruitassistant/app.py
+++ recruitassistant/app.py
@@ -1,11 +1,26 @@
 import time
 from flask import Flask
 from flask import jsonify 
+import firebase_admin
+from firebase_admin import db
+
 
 app = Flask(__name__)
+
+firebase_admin.initialize_app(options={
+    'databaseURL': 'https://recruitassistant-fe71e.firebaseio.com/'
+})
+RADB = db.reference('recruitassistant')
+
 
 @app.route('/time')
 def get_current_time():
     data = {'time': 10000}
     return jsonify(data)
+
+@app.route('/testlogin')
+def test_login():
+    data = {'username': 'hi'}
+    d1 = RADB.push(req)
+    return flask.jsonify({'id': d1.key}), 201
 
