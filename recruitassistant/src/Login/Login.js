392eaf38a1422aee3cd80920e07bfd963281fec0
michaevelli <michael.pc168@gmail.com>
2020-10-01 17:28:34 +1000

    mrege conflicts

--- recruitassistant/src/Login/Login.js
+++ recruitassistant/src/Login/Login.js
@@ -4,11 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import {Card, Container} from 'react-bootstrap';
 import axios from "axios";
 
-<<<<<<< HEAD
 export const submitLogin="http://localhost:5000/login"
-=======
-export const submitLogin="http://127.0.0.1:5000/login"
->>>>>>> firebaseintegration
 
 function Login() {
 	const [email, setEmail] = useState("");
@@ -23,10 +19,6 @@ function Login() {
 			email: email,
 			password: password
 		}
-<<<<<<< HEAD
-
-=======
->>>>>>> firebaseintegration
 		console.log(ndata);
 		
 		
@@ -45,12 +37,8 @@ function Login() {
 
 			})
 
-<<<<<<< HEAD
 	}
 
-=======
-	  }
->>>>>>> firebaseintegration
 	
 
 
