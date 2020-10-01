dd414f15767f240feb814756d0a97f7d32b1fc9e
michaevelli <michael.pc168@gmail.com>
2020-10-01 17:19:22 +1000

    error state on failed login

--- recruitassistant/src/Login/Login.js
+++ recruitassistant/src/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { TextField, Button } from "@material-ui/core";
+import { withStyles } from '@material-ui/core/styles';
 import {Card, Container} from 'react-bootstrap';
 import axios from "axios";
 
@@ -8,6 +9,7 @@ export const submitLogin="http://127.0.0.1:5000/login"
 function Login() {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [errorStatus, setErrorStatus] = useState("");
 
 
 	async function handleSubmit(e) {
@@ -29,7 +31,9 @@ function Login() {
 			.catch(function(error){
 				console.log("error:")
 				console.log(error.response)
-				//add to html to display error
+				document.getElementById("error").innerText = error.response.data.message
+				setErrorStatus("True")
+
 			})
 
 	  }
@@ -41,15 +45,15 @@ function Login() {
 			<Container style={{'textAlign': 'center'}}>
 				<h1>Recruit Assistant</h1>
 				<b>Sign In</b>
-
+				<div id="error" style={{color: 'red'}}></div>
 				<form onSubmit={handleSubmit}>
-					<TextField id="email" label="Email" value={email} onChange={e=>setEmail(e.target.value)}></TextField>
-					<br></br>
-					<TextField id="password" label="Password" type="password" value={password} onChange={e=>setPassword(e.target.value)}></TextField>
-					<br></br>
+					<TextField label="Email" value={email} error={errorStatus} onChange={e=>setEmail(e.target.value)}></TextField>
+					<br/>
+					<TextField label="Password" type="password" value={password} error={errorStatus} onChange={e=>setPassword(e.target.value)}></TextField>
+					<br/>
 					<Button type="submit">Log In</Button>
 				</form>
-				<br></br>
+				<br/>
 			
 				Don't have an account? <a href="/signup">Sign Up</a>
 			</Container>
