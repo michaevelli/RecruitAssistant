303d3c2d4b4b05abab8b9ebdaeb36bf00cdd0fd1
beccaD6 <becca.diaz6@gmail.com>
2020-10-01 10:26:21 +1000

    split into components

---
+++ recruitassistant/src/SharedComponents/SideMenu.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+export default function SideMenu(props) {
+   
+    return(
+        <div style={{'height': '100vh','backgroundColor': "#DEDEDE" }}>
+                <nav class="navbar " style={{'position':'fixed'}}>
+                <ul class="navbar-nav" >
+               
+               {
+                   props.random.map( (entry,index)=>
+                     (<li class="nav-item">
+                    <a class="nav-link" href="#">{entry}</a>
+                    </li>)
+                   )
+               }
+                </ul> 
+                </nav>
+                </div>
+
+    );
+}
