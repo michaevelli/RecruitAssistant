2b4a3fe9b317fc36643d18fa536a21b17603751e
beccaD6 <becca.diaz6@gmail.com>
2020-10-02 16:13:50 +1000

    added search fields

--- recruitassistant/src/JobSeekerComponents/JobSeekerDashboard.js
+++ recruitassistant/src/JobSeekerComponents/JobSeekerDashboard.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import  'bootstrap/dist/css/bootstrap.css';
 import {Link, Button, Grid,Card,CardContent,CardActions ,TextField,FormControl,InputLabel,MenuItem,Select} from "@material-ui/core";
-import {Container,Col,Row} from 'react-bootstrap';
+import {Form,Container,Col,Row} from 'react-bootstrap';
 import Typography from '@material-ui/core/Typography';
 import TitleBar from "../SharedComponents/TitleBar.js";
 import SideMenu from "../SharedComponents/SideMenu.js";
 
 
 export default function JobSeekerDashboard() {
+
+    const [searchString,setSearchString] = useState('');
+    const [location,setLocation] = useState('');
+    const [jobType,setJobType] = useState('');
+    const [experienceLevel,setExperienceLevel] = useState('');
+    const [salary,setSalary] = useState('');
+
     const handleSubmit= async (event) =>{
         event.preventDefault();
         //TODO
@@ -36,56 +43,119 @@ export default function JobSeekerDashboard() {
                 }}>
 
               
-                <Col sm={2}  >
-                    <SideMenu random={['JobSeeker Dashboard','Your Applications','FAQ']}/>
-                </Col >
-                <Col>
-                    <form onSubmit={handleSubmit}>
-                        <div style={{ 
-                            display: 'flex', 
-                            flexWrap: 'wrap',
-                            justifyContent: "center"}}>
-                    
-                        <TextField
-                            style={{ margin: 8 }}
-                            placeholder="Job Title, Company,Skills"
-                            margin="normal"
-                            InputLabelProps={{
-                            shrink: true,
-                            }}
-                            variant="outlined"
-                            />
-                            <TextField style={{ margin: 8 }}
-                                placeholder="Location"
-                                margin="normal"
-                                InputLabelProps={{
-                                shrink: true,
-                                }}
-                                variant="outlined"
-                             />
+            <Col sm={2}  >
+                <SideMenu random={['JobSeeker Dashboard','Your Applications','FAQ']}/>
+            </Col >
+            <Col>
+
+                <Typography variant="h4"  style={{color: 'black', textAlign: "center",margin:20 }}>
+                    Job Search
+                </Typography>
+
+                <Form onSubmit={handleSubmit}>                 
+                <Col xs={12} style={{ 
+                        display: 'flex', 
+                        flexWrap: 'wrap',
+                        justifyContent: "center"}} >
+                    <TextField
+                         onChange={ (event) => setSearchString(event.target.value)}
+                        style={{ margin: 8 }}
+                        placeholder="Job Title, Company,Skills"
+                        margin="normal"
+                        InputLabelProps={{
+                        shrink: true,
+                        }}
+                        variant="outlined"
+                        />
+                    <TextField 
+                        onChange={ (event) => setLocation(event.target.value)}
+                        style={{ margin: 8 }}
+                        placeholder="Location"
+                        margin="normal"
+                        InputLabelProps={{
+                        shrink: true,
+                        }}
+                        variant="outlined"
+                        />
+                    <Button type="submit" variant="contained" style={{margin:10}}>
+                        Search
+                    </Button>            
+                 </Col>
 
 
-                    <FormControl variant="outlined" style={{ margin: 8 }}>
 
-                    <InputLabel>Job-type</InputLabel>
+                 <Col xs={12} style={{ 
+                        display: 'flex', 
+                        flexWrap: 'wrap',
+                        justifyContent: "center"}}>
+                <FormControl variant="outlined" style={{ margin: 8 }}>
+
+                <InputLabel>Job-type</InputLabel>
                     <Select
                         value="job type"
-                        onChange={handleChange}
+                        onChange={ (event) => setJobType(event.target.value)}
                         label="Job-type"
                     >
-                        <MenuItem value="">
-                            <em>None</em>
-                        </MenuItem>
-                        <MenuItem value='Part-time'>Part-time</MenuItem>
-                        <MenuItem value='Full-time'>Full-time</MenuItem>
-                        <MenuItem value='Casual'>Casual</MenuItem>
-                    </Select>
-                    </FormControl>
-                    </div>
-                    </form>        
+                    <MenuItem value="">
+                        <em>n/a</em>
+                    </MenuItem>
+                    <MenuItem value='Part-time'>Part-time</MenuItem>
+                    <MenuItem value='Full-time'>Full-time</MenuItem>
+                    <MenuItem value='Casual/Vacation'>Casual/Vacation</MenuItem>
+                    <MenuItem value='Temp/Contract'>Temp/Contract</MenuItem>
+                </Select>
+                </FormControl>
+
+                <FormControl variant="outlined" style={{ margin: 8 }}>
+                <InputLabel>Experience Level</InputLabel>
+                  <Select
+                        value="job type"
+                        onChange={ (event) => setExperienceLevel(event.target.value)}
+                        label="Job-type"
+                    >
+                    <MenuItem value="">
+                        <em>n/a</em>
+                    </MenuItem>
+                    <MenuItem value='Internship'>Internship</MenuItem>
+                    <MenuItem value='Entry level'>Entry level</MenuItem>
+                    <MenuItem value='Assosciate'>Assosciate</MenuItem>
+                    <MenuItem value='Mid-Senior level'>Mid-Senior level</MenuItem>
+                    <MenuItem value='Director'>Director</MenuItem>
+                    <MenuItem value='Executive'>Executive</MenuItem>
+                </Select>
+                </FormControl>
+                  
+                <FormControl variant="outlined" style={{ margin: 8 }}>
+                <InputLabel>Salary p.a</InputLabel>
+                <Select
+                    value="salary"
+                    onChange={ (event) => setSalary(event.target.value)}
+                    label="Salary"
+                >
+                <MenuItem value="">
+                    <em>n/a</em>
+                </MenuItem>
+                <MenuItem value='0'>0k</MenuItem>
+                <MenuItem value='10'>10k+</MenuItem>
+                <MenuItem value='20'>20k+</MenuItem>
+                <MenuItem value='30'>30k+</MenuItem>
+                <MenuItem value='40'>40k+</MenuItem>
+                <MenuItem value='50'>50k+</MenuItem>
+                <MenuItem value='60'>60k+</MenuItem>
+                <MenuItem value='70'>70k+</MenuItem>
+                <MenuItem value='80'>80k+</MenuItem>
+                <MenuItem value='100'>100k+</MenuItem>
+                <MenuItem value='120'>120k+</MenuItem>
+                <MenuItem value='150'>150k+</MenuItem>
+                </Select>
+                </FormControl>
 
                 </Col>
                 
+                </Form>        
+               
+            </Col>
+            
         </Row>
         
       
