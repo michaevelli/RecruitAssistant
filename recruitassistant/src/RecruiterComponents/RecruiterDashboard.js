f30d86698ba184046cb57fd73d68df1d61a7f3b0
beccaD6 <becca.diaz6@gmail.com>
2020-10-02 10:39:18 +1000

    change buttons to links changes

--- recruitassistant/src/RecruiterComponents/RecruiterDashboard.js
+++ recruitassistant/src/RecruiterComponents/RecruiterDashboard.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import  'bootstrap/dist/css/bootstrap.css';
-import { Button, Grid,Card,CardContent,CardActions } from "@material-ui/core";
+import {Link, Button, Grid,Card,CardContent,CardActions } from "@material-ui/core";
 import {Container,Col,Row} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import TitleBar from "../SharedComponents/TitleBar.js";
 import SideMenu from "../SharedComponents/SideMenu.js";
@@ -28,7 +27,7 @@ export default function RecruiterDashboard() {
                     <Col sm={2}  >
                     <SideMenu random={['Recruiter Dashboard','FAQ']}/>
                     </Col >
-                    <Col sm={8}>
+                    <Col sm={9}>
                     <div className="card-deck"  style={{ display: 'flex', flexWrap: 'wrap',justifyContent: 'left'}}>
                   
                         <Card style={{margin: 30, height: 150, width:250}}>
@@ -44,7 +43,28 @@ export default function RecruiterDashboard() {
                                 <Typography color="textSecondary">
                                     status
                                 </Typography>
-                                <Button size="small">View applications</Button>
+                                <Link href="#" style={{marginLeft: 30}} >
+                                        View applications
+                                </Link>
+                            </CardActions>
+                    </Card>
+
+                    <Card style={{margin: 30, height: 150, width:250}}>
+                            <CardContent>                          
+                                <Typography variant="h5" component="h2">
+                                    Job title
+                                </Typography>
+                                <Typography color="textSecondary">
+                                    Company | City
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Typography color="textSecondary">
+                                    status
+                                </Typography>
+                                <Link href="#" style={{marginLeft: 30}} >
+                                        View applications
+                                </Link>
                             </CardActions>
                     </Card>
                 
@@ -64,8 +84,30 @@ export default function RecruiterDashboard() {
                             <Typography color="textSecondary">
                                     status
                             </Typography>
-                            <Button size="small">View applications</Button>
+                            <Link href="#" style={{marginLeft: 30}} >
+                                        View applications
+                                </Link>
                         </CardActions>
+                    </Card>
+
+               
+                    <Card variant="outlined" style={{margin: 30, height: 150, width:250, borderColor: 'black' }}>
+                            <CardContent>                          
+                                <Typography variant="h5" component="h2">
+                                    Alternate card style
+                                </Typography>
+                                <Typography color="textSecondary">
+                                    Company | City
+                                </Typography>
+                            </CardContent>
+                            <CardActions >
+                                <Typography color="textSecondary" >
+                                    status
+                                </Typography>
+                                <Link href="#" style={{marginLeft: 30}} >
+                                        View applications
+                                </Link>
+                            </CardActions>
                     </Card>
                     </div>
                     </Col>
