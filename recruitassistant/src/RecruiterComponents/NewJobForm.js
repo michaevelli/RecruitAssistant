f8132a0e52e2b76217bb936a9de3ada3a9e997f2
beccaD6 <becca.diaz6@gmail.com>
2020-10-08 15:59:44 +1100

    form layout, not dynamic yet

--- recruitassistant/src/RecruiterComponents/NewJobForm.js
+++ recruitassistant/src/RecruiterComponents/NewJobForm.js
@@ -1,17 +1,26 @@
 import React, { useState,useEffect } from "react";
 import  'bootstrap/dist/css/bootstrap.css';
-import {Link,Slider, Grid,Card,CardContent,Button,CardActions ,TextField,FormControl,InputLabel,MenuItem,Select} from "@material-ui/core";
-import {Form,Container,Col,Row,Collapse} from 'react-bootstrap';
+
+import {Link, Input,InputAdornment, Grid,Button,FormControl,InputLabel,TextField} from "@material-ui/core";
+import {Form,Container,InputGroup,Col,Row,Collapse} from 'react-bootstrap';
 import Typography from '@material-ui/core/Typography';
 import TitleBar from "../SharedComponents/TitleBar.js";
 import SideMenu from "../SharedComponents/SideMenu.js";
 import axios from "axios";
 
+
 export const jobUrl="http://localhost:5000/jobAdverts/"
 
 export default function NewJobForm() {
-    
-    
+	
+	//for date picker (closing date)
+	//More info: https://material-ui.com/components/pickers/
+    const [selectedDate, setSelectedDate] = useState(new Date('2014-08-18T21:11:54'));
+	const handleDateChange = (date) => {
+		setSelectedDate(date);
+	};
+
+
     //error msg or successful job creation
     const [response,setResponse]=useState('')
 	const [title,setTitle] = useState('');
@@ -21,12 +30,17 @@ export default function NewJobForm() {
     const [location,setLocation] = useState('');
     const [jobType,setJobType] = useState('');
     const [experienceLevel,setExperienceLevel] = useState('');
-    //salary range units are in k/$1000
-	const [salaryRange,setSalaryRange] = useState([0,10]);
+    //salary units in units k/$1000
+	const [salary,setSalary] = useState(100);
 	const [closingDate,setClosingDate] = useState('');
-	const [skills,setSkills] = useState('');
-	const [requiredDoc,setRequiredDoc] = useState('');
-    
+	const [skills,setSkills] = useState(['python','c++']);
+	const [requiredDoc,setRequiredDoc] = useState([]);
+	
+	const addSkill= () =>{
+		setSkills(prevSkills => [...prevSkills, ''])
+	}
+	
+
     //tTODO
 	const postJob = async () => {
         const url = jobUrl
@@ -56,11 +70,11 @@ export default function NewJobForm() {
 						{'text':'FAQ','href':'#','active': false}]}/>
 				</Col>
 
-				<Col sm="9">
+				<Col sm="10" >
 					<Typography variant="h4"  style={{color: 'black', textAlign: "center",margin:20 }}>
 						Create New Job
 					</Typography>
-                    <Form onSubmit={handleSubmit}>
+                    <Form onSubmit={handleSubmit} style={{marginLeft:'15%'}}>
                        
                         <Form.Group controlId="title">
 							<Form.Label column sm={2}>
@@ -68,9 +82,7 @@ export default function NewJobForm() {
 							</Form.Label>
 							<Col sm={10}>
 								<Form.Control placeholder="Title"
-									onChange={ (event) => {console.log(event.target.value)
-									setTitle(event.target.value)
-								}} />
+									onChange={ (event) => setTitle(event.target.value)} />
 							</Col>
 						</Form.Group>
 						<Form.Group controlId="company">
@@ -78,7 +90,8 @@ export default function NewJobForm() {
 							Company
 							</Form.Label>
 							<Col sm={10}>
-								<Form.Control placeholder="Company" />
+								<Form.Control placeholder="Company"
+								onChange={ (event) => setCompany(event.target.value)}/>
 							</Col>
 						</Form.Group>
 
@@ -87,7 +100,8 @@ export default function NewJobForm() {
 							Location
 							</Form.Label>
 							<Col sm={10}>
-								<Form.Control placeholder="Location" />
+								<Form.Control placeholder="Location"
+								onChange={ (event) => setLocation(event.target.value)}/>
 							</Col>
 						</Form.Group>
 
@@ -95,7 +109,8 @@ export default function NewJobForm() {
 							<Form.Label column sm={2}>
 							Description</Form.Label>
 							<Col sm={10}>
-							<Form.Control as="textarea" rows="3" />
+								<Form.Control as="textarea" rows="3" 
+								onChange={ (event) => setDescription(event.target.value)}/>
 							</Col>
 						</Form.Group>
 
@@ -103,34 +118,109 @@ export default function NewJobForm() {
 						<Form.Label column sm={2}>
 							Job type</Form.Label>
 							<Col sm={10}>
-							<Form.Control as="select" 
-							onChange={e=>setJobType(e.target.value)} 
-							defaultValue="Choose...">
-							<option>Part-time</option>
-							<option>Full-time</option>
-							<option>Casual/Vacation</option>
-							<option>Temp/Contract</option>
-							
-							</Form.Control>
+								<Form.Control as="select" 
+								onChange={e=>setJobType(e.target.value)} 
+								defaultValue="Choose...">
+									<option>Part-time</option>
+									<option>Full-time</option>
+									<option>Casual/Vacation</option>
+									<option>Temp/Contract</option>								
+								</Form.Control>
 							</Col>	
 						</Form.Group>
 
-
-						<fieldset>
-							<Form.Group as={Row}>
-							<Form.Label as="legend" column sm={2}>
-								Qualifications
-							</Form.Label>
+						<Form.Group controlId="experienceLevel">
+						<Form.Label column sm={2}>
+							Experience Level</Form.Label>
 							<Col sm={10}>
+								<Form.Control as="select" 
+								onChange={e=>setExperienceLevel(e.target.value)} 
+								defaultValue="Choose...">
+									<option>Internship</option>
+									<option>Entry level</option>
+									<option>Associate</option>
+									<option>Mid-Senior level</option>	
+									<option>Director</option>	
+									<option>Executive</option>								
+								</Form.Control>
+							</Col>	
+						</Form.Group>
+
+						<Form.Group controlId="salary">
+							<Form.Label column sm={2}>
+							Salary (p.a)
+							</Form.Label>
 							
-							<Link style={{marginLeft: 30}} >
-							+ Add Qualification
-							</Link>
+							<Col sm={10}>
+								<InputGroup>
+									<InputGroup.Prepend>
+										<InputGroup.Text id="inputGroupPrepend">$</InputGroup.Text>
+									</InputGroup.Prepend>
+									<Form.Control placeholder=""
+									onChange={ (event) => setSalary(event.target.value)}/>
+									<Form.Text className="text-muted">
+									Please enter salary in units of K/$1000
+									</Form.Text>
+								</InputGroup>
+							</Col>
+						</Form.Group>
+
+						
+						<Form.Group controlId="closingDate">
+							<Form.Label column sm={2}>
+							Closing Date</Form.Label>
+							<Col sm={10}>
+								<TextField
+									id="closingDate"
+									type="date"
+									onChange={ (event) => setClosingDate(event.target.value)}
+								/>
+							</Col>
+						</Form.Group>
+							
+						<fieldset>
+						<Form.Group controlId="skills">
+							<Form.Label as="legend" column sm={2}>
+							Qualifications
+							</Form.Label>
+							<Button style={{marginLeft: 30}} onClick={ addSkill
+							}>
+								+ Add Qualification
+							</Button>
+							<Col sm={10}>
+							{
+								skills.map( (s,indx) =>(
+								<Form.Group controlId="skill">
+									<Col sm={10}>
+										<Form.Control placeholder={s}/>
+									</Col>
+								</Form.Group>
+								))
+							}
+								
 
 							</Col>
-							</Form.Group>
+						</Form.Group>
 						</fieldset>
 
+						<fieldset>
+						<Form.Group controlId="requiredDocs">
+							<Form.Label as="legend" column sm={2}>
+							Required Documents
+							</Form.Label>
+							<Col sm={10}>
+								
+								<Link style={{marginLeft: 30}} >
+								+ Add Document type
+								</Link>
+
+							</Col>
+						</Form.Group>
+						</fieldset>
+
+						<Button variant="contained" color="primary" type="submit" style={{margin: 20}}>
+						Create New Job
+						</Button>    
 
 						
 				
