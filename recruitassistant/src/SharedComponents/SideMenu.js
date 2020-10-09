9377f056f73663e3284a85d237bc646efb068c40
beccaD6 <becca.diaz6@gmail.com>
2020-10-09 14:04:31 +1100

    fix minor issue with side nav bar not scrolling fully

--- recruitassistant/src/SharedComponents/SideMenu.js
+++ recruitassistant/src/SharedComponents/SideMenu.js
@@ -1,33 +1,28 @@
 import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 
-// TODO https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
-//make sticky - currently on some pages it disappears if you scroll too far down
+
 export default function SideMenu(props) {
    
     return(
-        <div style={{'height': '100vh','backgroundColor': "#DEDEDE"}}>
-                <nav class="navbar " style={{'position':'fixed'}}>
+        <div style={{'height': '100%','backgroundColor': "#DEDEDE" }}>
+            <nav class="navbar sticky" style={{'position':'fixed'}}>
                 <ul class="navbar-nav" >
-
-               {
-                   props.random.map( (entry,index)=>
-
-  
-                     (<li class="nav-item" >
-                    <a class="nav-link"
-                     href={entry.href}
-                     style={ entry.active ?{color: '#348360' , fontWeight:'bold' }: { color: '#348360' }  }
-                    >{entry.text}</a>
-                    </li>))
-                    
-                   
-               }
-            
+                    {
+                    props.random.map( (entry,index)=>(
+                        <li class="nav-item" >
+                            <a class="nav-link"
+                            href={entry.href}
+                            style={ entry.active ?{color: '#348360' , fontWeight:'bold' }: { color: '#348360' }  }
+                            >
+                            {entry.text}
+                            </a>
+                        </li>
+                        ))
+                    }  
                 </ul> 
-                </nav>
-                
-                </div>
+            </nav>
+        </div>
 
     );
 }
