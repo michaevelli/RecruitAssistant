07a3c595b03c1c16c0d128660fa92dbe5064d52d
beccaD6 <becca.diaz6@gmail.com>
2020-10-21 14:31:26 +1100

    allow nested links

--- recruitassistant/src/SharedComponents/SideMenu.js
+++ recruitassistant/src/SharedComponents/SideMenu.js
@@ -9,20 +9,30 @@ export default function SideMenu(props) {
             <nav class="navbar sticky" style={{'position':'fixed'}}>
                 <ul class="navbar-nav">
                     {
-                    props.random.map( (entry,index)=>(
+                    props.random.map( (entry,index)=>{
+                        return (
                         <li class="nav-item" >
-                            <a class="nav-link"
+                           <a class="nav-link"
                             href={entry.href}
                             style={ entry.active ?{color: '#348360' , fontWeight:'bold' }: { color: '#348360' }  }
                             >
                             {entry.text}
+
+                            {entry.nested && entry.nested.map((nested_link,index) => (
+
+                                <a class="nav-link"
+                                href={nested_link.href}
+                                style={ nested_link.active ?{color: '#348360', marginLeft: 15,fontWeight:'bold' }: {marginLeft: 15, color: '#348360' }  }
+                                >
+                                {nested_link.text}</a>
+                            ))}
+
                             </a>
                         </li>
-                        ))
-                    }  
-                </ul> 
+                        )}
+                    )}
+                </ul>          
             </nav>
         </div>
-
-    );
+        )
 }
