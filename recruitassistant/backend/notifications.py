02f8f126a91c749c7de22be069855592d53cb004
Brendon <bbrendonshi@gmail.com>
2020-11-04 18:41:25 +1100

    removed old code

--- recruitassistant/backend/notifications.py
+++ recruitassistant/backend/notifications.py
@@ -3,7 +3,6 @@ from .init_app import app, ref, pb
 import uuid
 from datetime import datetime
 
-
 # receives type of notification (i.e. interview/offer/counteroffer)
 # function is called from hooking into interview/offer/counteroffer functions
 # sets information in database under notification node
@@ -25,22 +24,6 @@ def notify(data):
 	except Exception as e:		
 		return jsonify({"message": str(e)}), 400
 
-# records notifications viewed by the user
-# update notifications viewed by the user
-# @app.route('/recnotif', methods=["POST"])
-# def rec_notif():
-# 	data = request.get_json()
-# 	try:
-# 		ref.child("seenNotifs").update({
-# 			data["uid"] : {
-# 				'list': data["list"]
-# 			}
-# 		})
-# 		return jsonify({'message': "recorded notifications viewed"}),200
-# 	except Exception as e:
-# 		print(e)
-# 		return jsonify({"message": str(e)}), 400
-
 # should update database with items "seen" status to true
 @app.route('/recnotif', methods=["POST"])
 def rec_notif():
@@ -53,19 +36,6 @@ def rec_notif():
 	except Exception as e:
 		return jsonify({"message": str(e)}), 400
 
-# @app.route('/recnotif', methods=["GET"])
-# def get_seen():
-# 	uid = request.args.get("uid")
-# 	db = pb.database()
-# 	node_exist = db.shallow().get().val()
-# 	if "seenNotifs" in list(node_exist):
-# 		d1 = db.child("seenNotifs").order_by_key().equal_to(uid).get()
-# 		if list(d1) != []:
-# 			return jsonify({"exists": "true", "data": list(d1.val().items())}),200
-# 		else:
-# 			return jsonify({"exists": "true", "data": []}),200
-# 	else:
-# 		return jsonify({"message": "no stored notifications"}), 200
 
 # function removes notifications from user clicking on x button
 @app.route('/remnotif', methods=["POST"])
