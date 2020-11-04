deac7295f625341332efc2a8e09173c9e72ee84e
Brendon <bbrendonshi@gmail.com>
2020-11-04 18:40:05 +1100

    reworked notifications to track status on the notification object, fixed a couple bugs as well

--- recruitassistant/backend/notifications.py
+++ recruitassistant/backend/notifications.py
@@ -18,6 +18,7 @@ def notify(data):
 				"object_id" : data["obj_id"],
 				"date_time": date_time,
 				"url" : data["url"],
+				"seen" : False,
 				}
 			)
 		return jsonify({'message': f'Successfully created notif {notif_id}'}),200
@@ -25,33 +26,46 @@ def notify(data):
 		return jsonify({"message": str(e)}), 400
 
 # records notifications viewed by the user
+# update notifications viewed by the user
+# @app.route('/recnotif', methods=["POST"])
+# def rec_notif():
+# 	data = request.get_json()
+# 	try:
+# 		ref.child("seenNotifs").update({
+# 			data["uid"] : {
+# 				'list': data["list"]
+# 			}
+# 		})
+# 		return jsonify({'message': "recorded notifications viewed"}),200
+# 	except Exception as e:
+# 		print(e)
+# 		return jsonify({"message": str(e)}), 400
+
+# should update database with items "seen" status to true
 @app.route('/recnotif', methods=["POST"])
 def rec_notif():
 	data = request.get_json()
+	seen_list = data["list"]
 	try:
-		ref.child("seenNotifs").update({
-			data["uid"] : {
-				'list': data["list"]
-			}
-		})
-		return jsonify({'message': "recorded notifications viewed"}),200
+		for i in seen_list:
+			ref.child("notif").child(data["uid"]).child(i).child("seen").set(True)
+		return jsonify("message", "fine"), 200
 	except Exception as e:
-		print(e)
 		return jsonify({"message": str(e)}), 400
 
-@app.route('/recnotif', methods=["GET"])
-def get_seen():
-	uid = request.args.get("uid")
-	db = pb.database()
-	node_exist = db.shallow().get().val()
-	if "seenNotifs" in list(node_exist):
-		d1 = db.child("seenNotifs").order_by_key().equal_to(uid).get()
-		if list(d1) != []:
-			return jsonify({"exists": "true", "data": list(d1.val().items())}),200
-		else:
-			return jsonify({"exists": "true", "data": []}),200
-	else:
-		return jsonify({"message": "no stored notifications"}), 200
+# @app.route('/recnotif', methods=["GET"])
+# def get_seen():
+# 	uid = request.args.get("uid")
+# 	db = pb.database()
+# 	node_exist = db.shallow().get().val()
+# 	if "seenNotifs" in list(node_exist):
+# 		d1 = db.child("seenNotifs").order_by_key().equal_to(uid).get()
+# 		if list(d1) != []:
+# 			return jsonify({"exists": "true", "data": list(d1.val().items())}),200
+# 		else:
+# 			return jsonify({"exists": "true", "data": []}),200
+# 	else:
+# 		return jsonify({"message": "no stored notifications"}), 200
 
 # function removes notifications from user clicking on x button
 @app.route('/remnotif', methods=["POST"])
@@ -80,9 +94,11 @@ def check_notif():
 			d2 = db.child("notif").child(data["uid"]).order_by_child("date_time").get()
 			return_l = list(d2.val().items())
 			return_l.reverse()
+			# d2 = db.child("notif").child(data["uid"]).get()
+			# return_l = list(d2.val().items())
 
 			return jsonify({"exists": "true", "data": return_l}),200
 		else:
 			return jsonify({"exists": "true", "data": []}),200
 	else:
-		return jsonify({"message": "no notifications"}), 200
+		return jsonify({"message": "no notifications", "data": []}), 200
