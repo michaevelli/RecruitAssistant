7c29822ef36132a0b7d9d5bd9c511ac44b790311
Brendon <bbrendonshi@gmail.com>
2020-11-08 12:35:00 +1100

    delete users complete

--- recruitassistant/backend/admin.py
+++ recruitassistant/backend/admin.py
@@ -24,6 +24,7 @@ def del_post():
         remove_list(ref.child('interviews').order_by_child('job_id').equal_to(post_id).get(), "interviews")
 
         # delete associated notifications
+        # remove_list(ref.child('notif').order_by_child('object_id').equal_to(post_id).get(), "notif")
 
         # delete associated applications
         db.child('jobApplications').child(post_id).remove()
@@ -41,7 +42,6 @@ def del_post():
 def get_users():
     try:
         user_list = list(ref.child('user').order_by_key().get().items())
-        print(user_list)
         return jsonify({'message': 'Success', 'data' : user_list}), 200
     except Exception as e:
         print(e)
@@ -53,13 +53,31 @@ def del_user():
     json_data = request.get_json()
     user_id = json_data["uid"]
     try:
-        # remove offers
-        # remove interviews
-        # remove applications
-        # remove job posts
+        if(json_data["type"] == "jobseeker"):
+            # remove offer
+            remove_list(ref.child('offer').order_by_child('jobseeker_id').equal_to(user_id).get(),'offer')
+            # remove interviews
+            remove_list(ref.child('interviews').order_by_child('jobseeker_id').equal_to(user_id).get(), 'interviews')
+            # remove application
+            remove_list(ref.child('jobApplications').order_by_child('jobseeker_id').equal_to(user_id).get(), 'jobApplications')
+        elif(json_data["type"]=="recruiter"):
+            # remove offers
+            remove_list(ref.child('offer').order_by_child('recruiter_id').equal_to(user_id).get(),'offer')
+
+            # remove interviews
+            remove_list(ref.child('interviews').order_by_child('employer_id').equal_to(user_id).get(), 'interviews')
+
+            # remove job advertisement
+            remove_list(ref.child('jobAdvert').order_by_child('recruiter_id').equal_to(user_id).get(), 'jobAdvert')
+        
         # remove notifications
+        child_exist = db.child("notif").shallow().get().val()
+        if(user_id in list(child_exist)):
+            db.child('notif').child(user_id).remove()
+        
         # remove user
-        db.child(user).child(user_id).remove()
+        db.child('user').child(user_id).remove()
         return jsonify({'message': f'Successfully deleted user {user_id} and associated items'}),200
     except Exception as e:
+        print(e)
         return jsonify({"message": str(e)}), 400
