a2e3dab51818a5a5d2cf353aa69e5638bf186c33
beccaD6 <becca.diaz6@gmail.com>
2020-11-08 13:06:34 +1100

    add some extra validation to form

--- recruitassistant/src/Authentication/SignUpJobSeeker.js
+++ recruitassistant/src/Authentication/SignUpJobSeeker.js
@@ -19,48 +19,54 @@ function SignUpJobSeeker(props) {
 
 	async function handleSubmit(e) {
 		e.preventDefault()
-		setErrorMessage("")
-		if (password === repassword) {
-			const ndata = {
-				first_name: first_name,
-				last_name: last_name,
-				email: email,
-				password: password,
-				type: "jobseeker"
-			}
-			console.log(ndata)
-			axios.post(submitSignUp, ndata)
-				.then(function(response) {
-					console.log("response:", response)
-					alert("successfully created account")
-					history.push("/login")
-				})
-				.catch(function(error){
-					console.log("error:", error.response)
-					setErrorMessage(error.response.data.message)
-					
-				})
-		} else {
+		
+		if (password !== repassword) {
 			setErrorMessage("Passwords do not match")
-		} 
+			return false
+		}
+		const ndata = {
+			first_name: first_name,
+			last_name: last_name,
+			email: email,
+			password: password,
+			type: "jobseeker"
+		}
+		console.log(ndata)
+		axios.post(submitSignUp, ndata).then(function(response) {
+			console.log("response:", response)
+			alert("successfully created account")
+			history.push("/login")
+		})
+		.catch(function(error){
+			console.log("error:", error.response)
+			const msg=error.response.data.message
+			if (msg ==="Error while calling Auth service (INVALID_EMAIL)."){
+				setErrorMessage("Invalid email address.")
+			}else if(msg==="The user with the provided email already exists (EMAIL_EXISTS)."){
+				setErrorMessage("This email already exists.")
+			}else{
+				setErrorMessage(msg)
+			}
+		})
+		
 	}
-	
-	
+
+
 	return (
 		<div>
 			<Container style={{'textAlign': 'center'}}>
 				<img src={logo} style={{ width:400, height:100, marginBottom:20,marginTop:20, marginLeft:20}}></img>
 				
 				<form onSubmit={handleSubmit}>
-					<TextField label="First Name" value={first_name} onChange={e=>setFirstName(e.target.value)}/>
+					<TextField label="First Name" required value={first_name} onChange={e=>setFirstName(e.target.value)}/>
 					<br/>
-					<TextField label="Last Name" value={last_name} onChange={e=>setLastName(e.target.value)}/>
+					<TextField label="Last Name" required value={last_name} onChange={e=>setLastName(e.target.value)}/>
 					<br/>
-					<TextField label="Email" value={email} onChange={e=>setEmail(e.target.value)}/>
+					<TextField label="Email" required value={email} onChange={e=>setEmail(e.target.value)}/>
 					<br/>
-					<TextField label="Password" type="password" value={password} onChange={e=>setPassword(e.target.value)}/>
+					<TextField label="Password" required type="password" value={password} onChange={e=>setPassword(e.target.value)}/>
 					<br/>
-					<TextField label="Re-enter password" type="password" value={repassword} onChange={e=>setRepassword(e.target.value)}/>
+					<TextField label="Re-enter password" required type="password" value={repassword} onChange={e=>setRepassword(e.target.value)}/>
 					<br/>
 					<div id="error" style={{color: 'red'}}>{errorMessage}</div>
 					<Button type="submit" variant="contained" style={{margin:20}}> Sign Up </Button>
