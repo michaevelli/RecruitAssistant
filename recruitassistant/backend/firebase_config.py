5705b5c6895f217386351d500393f261601a3e77
Brendon <bbrendonshi@gmail.com>
2020-11-09 16:27:03 +1100

    secondary database added

--- recruitassistant/backend/firebase_config.py
+++ recruitassistant/backend/firebase_config.py
@@ -11,6 +11,16 @@ firebaseConfig = {
     "appId": "1:827071966465:web:7b2de65ce25425a5c93f00"
   }
 
+# firebaseConfig2 = {
+#     "apiKey": "AIzaSyCz63auke4l0tRlhIjJBH8jHtbOKuj63EU",
+#     "authDomain": "recruitassistant-v2.firebaseapp.com",
+#     "databaseURL": "https://recruitassistant-v2.firebaseio.com",
+#     "projectId": "recruitassistant-v2",
+#     "storageBucket": "recruitassistant-v2.appspot.com",
+#     "messagingSenderId": "508128916024",
+#     "appId": "1:508128916024:web:332e32d9d37099e30c6ccd"
+# }
+
 firebase = pyrebase.initalize_app(firebaseConfig)
 
 fireAuth = firebase.auth()
